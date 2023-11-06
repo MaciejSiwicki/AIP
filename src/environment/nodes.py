d0d57b0682ff88a0f2ed6c304b060180f200a627
Maciej Siwicki <msiw@thaumatec.com>
2023-11-06 14:04:16 +0100

    add ghost with spawn and eat

--- src/environment/nodes.py
+++ src/environment/nodes.py
@@ -28,6 +28,7 @@ class NodeGroup(object):
         self.createNodeTable(data)
         self.connectHorizontally(data)
         self.connectVertically(data)
+        self.homekey = None
 
     def readMazeFile(self, textfile):
         return np.loadtxt(textfile, dtype="<U1")
@@ -98,3 +99,25 @@ class NodeGroup(object):
     def render(self, screen):
         for node in self.nodesLUT.values():
             node.render(screen)
+
+    def createHomeNodes(self, xoffset, yoffset):
+        homedata = np.array(
+            [
+                ["X", "X", "+", "X", "X"],
+                ["X", "X", ".", "X", "X"],
+                ["+", "X", ".", "X", "+"],
+                ["+", ".", "+", ".", "+"],
+                ["+", "X", "X", "X", "+"],
+            ]
+        )
+
+        self.createNodeTable(homedata, xoffset, yoffset)
+        self.connectHorizontally(homedata, xoffset, yoffset)
+        self.connectVertically(homedata, xoffset, yoffset)
+        self.homekey = self.constructKey(xoffset + 2, yoffset)
+        return self.homekey
+
+    def connectHomeNodes(self, homekey, otherkey, direction):
+        key = self.constructKey(*otherkey)
+        self.nodesLUT[homekey].neighbors[direction] = self.nodesLUT[key]
+        self.nodesLUT[key].neighbors[direction * -1] = self.nodesLUT[homekey]
